ba489c64e5317b462b847b3c03754b16ef8f4be1
Pâmela Paesi Bieger <53712205+PamelaPaesi@users.noreply.github.com>
2022-12-04 13:21:30 -0300

    Finalizado teste pratico 1 e 2

--- Testes/src/main/java/org/example/ErroTestePraticoUm.java
+++ Testes/src/main/java/org/example/ErroTestePraticoUm.java
@@ -1,5 +1,4 @@
 package org.example;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -8,8 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.ArrayList;
 import java.util.List;
-
-public class TestePraticoUm {
+public class ErroTestePraticoUm {
     public static void main( String[] args ) throws InterruptedException {
         System.setProperty("web-driver.gecko.driver", "C:/Program Files/JetBrains/IntelliJ IDEA Community Edition 2022.3/bin/geckodriver.exe");
 
@@ -24,32 +22,15 @@ public class TestePraticoUm {
         driver.switchTo().window(abas.get(1));
         Thread.sleep(5000);
         driver.findElement(By.xpath("//button[@class='ctaColor']")).click();
-        driver.findElement(By.linkText("Não sei meu CEP")).click();
+        driver.findElement(By.id("mat-input-0")).sendKeys("92020303");
         Thread.sleep(5000);
-        driver.findElement(By.xpath("//div[@class='mat-form-field-flex ng-tns-c157-64']")).click();
-        Thread.sleep(5000);
-        driver.findElement(By.xpath("//mat-option[@id='mat-option-172']")).click();
-        driver.findElement(By.id("mat-input-6")).sendKeys("Pinhalzinho");
-        Thread.sleep(10000);
-        driver.findElement(By.xpath("//mat-option[@id='mat-option-176']")).click();
-        driver.findElement(By.id("mat-select-1")).click();
-        driver.findElement(By.id("mat-option-0")).click();
-        driver.findElement(By.id("mat-input-7")).sendKeys("Natal");
-        driver.findElement(By.xpath("//button[@class='modal__search-button']")).click();
-        Thread.sleep(5000);
-        WebElement m = driver.findElement(
-                By.xpath("//div[@class='modal__enderecos__container ng-star-inserted']")
-        );
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollTop = 100;", m);
-        Thread.sleep(5000);
-        driver.findElement(By.id("endereco0")).click();
         driver.findElement(By.id("mat-input-1")).sendKeys("20534248691");
         driver.findElement(By.id("mat-input-2")).sendKeys("Bernardo Pedro Henrique Juan Dias");
         driver.findElement(By.id("mat-input-3")).sendKeys("(21) 98891-4304");
         driver.findElement(By.id("mat-input-4")).sendKeys("bernardo_pedro_dias@edepbr.com.br");
-        driver.findElement(By.id("mat-input-5")).sendKeys("Iphone 11 pro max");
-        Thread.sleep(5000);
-        driver.findElement(By.xpath("//mat-option[@id='mat-option-179']")).click();
+        driver.findElement(By.id("mat-input-5")).click();
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//div[@class='mat-button-toggle-label-content']")).click();
+        System.out.println("Preenchimento do campo obrigatório");
     }
 }
---
+++ Testes/src/test/java/test/erroTestePraticoUm.java
@@ -0,0 +1,16 @@
+package test;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class erroTestePraticoUm {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
